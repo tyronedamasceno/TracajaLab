3858eb9cac2886969b2c91aa072f7cd508282eee
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-14 17:08:12 -0300

    SplashScreen working

---
+++ app/src/main/java/com/example/tyrone/tracajalab/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.tyrone.tracajalab;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showMainActivity();
+            }
+        }, 2000);
+    }
+
+    public void showMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
