4699df83a4c61b262fb62a95684f830a2be07056
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-16 10:24:25 -0300

    Welcome activity and app launcher icon ok

---
+++ app/src/main/java/com/example/tyrone/tracajalab/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.tyrone.tracajalab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void schedule(View view) {
+    }
+
+    public void receiveResults(View view) {
+    }
+
+    public void changeSchedule(View view) {
+    }
+}
