e49442c45107599f55156d2b7e729eef043f238e
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-16 09:30:38 -0300

    Splash and login screens finished

--- app/src/main/java/com/example/tyrone/tracajalab/SplashActivity.java
+++ app/src/main/java/com/example/tyrone/tracajalab/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends Activity {
     }
 
     public void showMainActivity() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
