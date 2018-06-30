c1f1c7eb86690bbcb883ca3908f9790282c3fafa
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-06-30 15:09:01 -0300

    Fixing domains

--- app/src/main/java/com/example/tyrone/tracajalab/Domain/Exame.java
+++ app/src/main/java/com/example/tyrone/tracajalab/Domain/Exame.java
@@ -1,16 +1,37 @@
 package com.example.tyrone.tracajalab.Domain;
 
+import java.util.Date;
+
 public class Exame {
 
     private String nome;
     private String orientacao;
     private String jejum;
     private String resultado;
+    private Date data;
+    private boolean pronto;
+
+    public boolean isPronto() {
+        return pronto;
+    }
+
+    public void setPronto(boolean pronto) {
+        this.pronto = pronto;
+    }
 
     public Exame(String nome, String orientacao, String jejum) {
         this.nome = nome;
         this.orientacao = orientacao;
         this.jejum = jejum;
+    }
+
+    public Exame (Date data) {
+        this.data = data;
+    }
+
+    public Exame (Date data, String resultado) {
+        this.data = data;
+        this.resultado = resultado;
     }
 
     public String getResultado() {
--- app/src/main/java/com/example/tyrone/tracajalab/Domain/Paciente.java
+++ app/src/main/java/com/example/tyrone/tracajalab/Domain/Paciente.java
@@ -4,18 +4,19 @@ import java.util.HashMap;
 
 public class Paciente {
 
-    private String nome;
-    private String login;
-    private String senha;
+    private String email;
     private HashMap<String, Exame> exames;
 
-    public Paciente(String nome, String login, String senha) {
-        this.nome = nome;
-        this.login = login;
-        this.senha = senha;
+    public Paciente(String email) {
+        this.email = email;
 
         if (exames == null) {
             exames = new HashMap<>();
         }
+    }
+
+    public Paciente(String email, HashMap<String, Exame> exames) {
+        this.email = email;
+        this.exames = exames;
     }
 }
