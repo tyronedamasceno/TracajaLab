4f2c1809be6c9aaa5a7632216fe525ac7e339384
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-07-01 21:54:42 -0300

    Creating results activity

---
+++ app/src/main/java/com/example/tyrone/tracajalab/Activities/ResultsActivity.java
@@ -0,0 +1,13 @@
+package com.example.tyrone.tracajalab.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
